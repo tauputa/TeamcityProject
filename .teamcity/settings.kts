c93f6cc2063c2bc3d9a4e9255b96a1a6e9d24fca
Alistair.Young <alistair.young@xero.com>
2022-08-17 21:03:19 +1200

    done

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-//import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 //import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs     
 
 version = "2021.1"
@@ -8,7 +8,27 @@ project {
         param("teamcity.ui.settings.readOnly", "true")
     }
     description = "Maven 3.6 java project forked from anewtodolist"
-//    buildType(NewList_Build)  
+    buildType(NewList_Build)  
 }
 
+object NewList_Build : BuildType({ 
+    id("Build_ID") 
+    name = "Build_Name" 
 
+    vcs {
+        root(DslContext.settingsRoot) 
+    }
+
+    steps {
+        maven { 
+            goals = "clean test" 
+            runnerArgs = "-Dmaven.test.failure.ignore=true" 
+            mavenVersion = bundled_3_6() 
+        }
+    }
+
+//    triggers {    
+//        vcs {    
+//        }
+//    }
+})
