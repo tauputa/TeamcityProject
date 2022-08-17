5ddb2b46b78a34ad66592f1ec20f2e598bfa6d97
Alistair.Young <alistair.young@xero.com>
2022-08-18 08:03:14 +1200

    done

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,100 +4,38 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2021.1"
 project {
-    params {
-        param("teamcity.ui.settings.readOnly", "true")
-    }
-    description = "Maven 3.6 java project forked from anewtodolist"
-    buildType(Clean)  
-    buildType(Package)  
-    buildType(UnitTest)
-    buildType(IntegrationTest)
-    sequential {
-        buildType(Clean)
+
+    val bts = sequential {
+        buildType(Maven("Build","clean compile","-Dmaven.test.failure.ignore=true","Build"))
 	parallel{
-		buildType(UnitTest)
-		buildType(IntegrationTest)
+        buildType(Maven("Unit","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test","Unit"))
+        buildType(Maven("Integration","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test","Integration"))
 	}
-        buildType(Package)
+        buildType(Maven("Package","clean package","-Dmaven.test.failure.ignore=true -DskipTests","Package"))
+    }.buildTypes()
+    bts.forEach{buildType(it)}
+    bts.Last().triggers{
+        vcs {
+            
+        }
     }
 }
 
-object UnitTest : BuildType({
-    id("UnitTest")
-    name = "Unit Test"
+class Maven(name:String,goals:String,runnerArgs:String?=null,ID: String): BuildType({
+    //id("UnitTest")
+    id(this.ID)
+    this.name = name
     vcs {
         root(DslContext.settingsRoot)
     }
     steps {
         maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test" 
-            mavenVersion = bundled_3_6()
-        }
-    }
-    requirements { 
-        contains("teamcity.agent.name", "linux")
-    }
-})
-
-object IntegrationTest : BuildType({
-    id("IntegrationTest")
-    name = "Integration Test"
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-    steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test" 
-            mavenVersion = bundled_3_6()
-        }
-    }
-    requirements {  
-        contains("teamcity.agent.name", "linux")
-    }
-})
-
-object Clean : BuildType({ 
-    id("Clean_ID") 
-    name = "Clean_Name" 
-
-    vcs {
-        root(DslContext.settingsRoot) 
-    }
-
-    steps {
-        maven { 
-            goals = "clean test" 
-            runnerArgs = "-Dmaven.test.failure.ignore=true" 
-            mavenVersion = bundled_3_6() 
-        }
-    }
-    requirements {
-        contains("teamcity.agent.name", "linux")
-    }
-})
-
-object Package : BuildType({
-    id("Package_ID")
-    name = "Package_Name"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean package"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            this.goals = goals
+            this.runnerArgs = runnerArgs
             mavenVersion = bundled_3_6()
         }
     }
     requirements {
         contains("teamcity.agent.name", "linux")
-    }
-    triggers {
-        vcs {
-        }
     }
 })
